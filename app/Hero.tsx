a3486c0f001eb2f84998414be90f226ee83b6728
cent kanayo <centkanayo@gmail.com>
2023-01-26 10:34:45 +0100

    Fix: adjusting hero screen

--- app/Hero.tsx
+++ app/Hero.tsx
@@ -100,7 +100,7 @@ const Hero = () => {
     setShowGrade(true);
   };
   return (
-    <main className="w-full relative  p-2 md:w-[500px] mx-auto">
+    <main className="relative  p-2  mx-auto">
       <div className=" border border-gray-400 bg-white p-4 rounded-md ">
         <div className="border-b-2 border-gray-200">
           <h1 className="text-2xl text-center text-blue-700 font-semibold mb-4 tracking-wide uppercase">
